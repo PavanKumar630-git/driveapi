91c3436db4d03b98ec59e0cf2c94e446afc8cd91
PavanKumar630-git <pavankumar.scarping@gmail.com>
2025-09-04 01:06:14 +0530

    Delete app directory

--- app/utils/db.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import create_engine
-
-DB_CONN = "mssql+pyodbc://sa:Server$#@54321@61.246.34.128/dropboxfastapi?driver=ODBC+Driver+17+for+SQL+Server"
-engine = create_engine(DB_CONN)
--- app/utils/ftp_client.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def upload_to_ftp(creds, localfolder, uploadfolder):
-    for root, _, files in os.walk(localfolder):
-        for f in files:
-            print(f"[FTP] Upload {os.path.join(root, f)} -> {uploadfolder}/{f}")
-
-def list_ftp_files(creds):
-    return [{"name": "ftp_file.txt", "size": 789, "is_folder": False, "cloud": "ftp"}]
